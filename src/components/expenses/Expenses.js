a838966b74bf67678ef090b9af8e671b9a83d627
rinarexhepi <rinnaarexhepi@gmail.com>
2023-08-14 18:59:23 +0200

    rendering items by year

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -6,9 +6,14 @@ import { useState } from "react";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -17,7 +22,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}

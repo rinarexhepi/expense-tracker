e8c8c8b438b1ba5734db41f9ec3b164cee92c29a
rinarexhepi <rinnaarexhepi@gmail.com>
2023-08-14 21:24:45 +0200

    finishing up the chart

--- src/components/expenses/ExpensesChart.js
+++ src/components/expenses/ExpensesChart.js
@@ -16,7 +16,7 @@ const ExpensesChart = (props) => {
     { label: "Dec", value: 0 },
   ];
 
-  for (const expense in props.expenses) {
+  for (const expense of props.expenses) {
     const expenseMonth = expense.date.getMonth(); //starting at 0 => Jan
     chartDataPoints[expenseMonth].value += expense.amount;
   }
